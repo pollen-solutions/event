5c3d6141f4f0024d0f1ae694dd01ad264e17253e
jordy-manner <jordy.manner@milkcreation.fr>
2021-07-07 17:14:37 +0200

    Initial commit

---
+++ src/TriggeredEventInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Pollen\Event;
+
+use League\Event\HasEventName;
+
+interface TriggeredEventInterface extends HasEventName
+{
+    /**
+     * Get the triggered event name.
+     *
+     * @return string
+     */
+    public function eventName(): string;
+
+    /**
+     * Get list of arguments passed to the associated listeners.
+     *
+     * @return array
+     */
+    public function eventArgs(): array;
+
+    /**
+     * Set list of arguments passed to the associated listeners.
+     *
+     * @param array $args
+     *
+     * @return static
+     */
+    public function setEventArgs(array $args): TriggeredEventInterface;
+}
