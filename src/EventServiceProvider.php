8126d9081df61c6b619c6c553bd15c4aa5abbd70
jordy-manner <jordy.manner@milkcreation.fr>
2022-03-29 15:25:29 +0200

    feat: evolution config

--- src/EventServiceProvider.php
+++ src/EventServiceProvider.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Pollen\Event;
 
-use Pollen\Container\ServiceProvider;
+use Pollen\Container\BootableServiceProvider;
+use Pollen\Kernel\Events\ConfigLoadEvent;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
-class EventServiceProvider extends ServiceProvider
+class EventServiceProvider extends BootableServiceProvider
 {
     protected $provides = [
         EventDispatcherInterface::class
@@ -15,10 +18,29 @@ class EventServiceProvider extends ServiceProvider
     /**
      * @inheritDoc
      */
+    public function boot(): void
+    {
+        try {
+            /** @var EventDispatcherInterface $event */
+            if ($event = $this->getContainer()->get(EventDispatcherInterface::class)) {
+                $event->subscribeTo('config.load', static function (ConfigLoadEvent $event) {
+                    if (is_callable($config = $event->getConfig('event'))) {
+                        $config($event->getApp()->get(EventDispatcherInterface::class), $event->getApp());
+                    }
+                });
+            }
+        } catch (NotFoundExceptionInterface|ContainerExceptionInterface $e) {
+            unset($e);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function register(): void
     {
         $this->getContainer()->share(EventDispatcherInterface::class, function () {
-            return new EventDispatcher([], $this->getContainer());
+            return new EventDispatcher($this->getContainer());
         });
     }
 }
